a4a2076e2598e23f595feedd1b17703aa2a6d540
a-zelenschi <a.zelenschi@gmail.com>
2025-10-28 04:32:37 +0200

    su

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById('fetchBtn');
 const messageDiv = document.getElementById('message');
 const switchSnakeBtn = document.getElementById('switchSnake');
+const serverURL = 'https://my-express-app-4mb3.onrender.com';
 
 // Buton pentru Snake
 switchSnakeBtn.addEventListener('click', () => {
@@ -10,7 +11,7 @@ switchSnakeBtn.addEventListener('click', () => {
 // Buton pentru mesaj de salut
 btn.addEventListener('click', async () => {
   try {
-    const response = await fetch('https://my-express-app-4mb3.onrender.com/salut'); // cerere către server
+    const response = await fetch(`${serverURL}/salut`); // cerere către server
     const text = await response.text();
     messageDiv.textContent = text;
   } catch (err) {
